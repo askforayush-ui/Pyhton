7242b2b8513f11eebdc3600240f630ae8f4fc102
Ayush Kumar <2405271@kiit.ac.in>
2026-04-22 15:14:54 +0530

    !

---
+++ two.py
@@ -0,0 +1,24 @@
+#String datatype
+food ="pizza"
+fruit = "banana"
+first_name="bro"
+print(first_name)
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your Favourite fruit is {fruit}")
+#Integer datatype
+age = 23
+quantity = 7
+print(f"Your age is {age}")
+print(f"your number is{quantity}")
+#float datatype
+price = 70.99
+print(f"your cost of the toy is ${price}")
+#boolean datatype
+is_student = False
+if is_student:
+    print("You are the student")
+else:
+    print("You are not the student")
+    is_student = True
+    print(is_student)
