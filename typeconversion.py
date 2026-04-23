63914f364e0e534cd314887239a8f69a2bd13839
Ayush Kumar <2405271@kiit.ac.in>
2026-04-23 18:55:22 +0530

    !

---
+++ typeconversion.py
@@ -0,0 +1,11 @@
+#here we will be learning about how the type casting is done in the pyhton language
+name ="Ayush"
+age = 21
+gpa = 9.1
+student =True
+print(type(student))
+print(type(name))
+print(type(age))
+print(type(gpa))
+student = str (student)
+print(type(student))
